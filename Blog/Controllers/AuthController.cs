13c7c0440713078c057dc1bd69b9e7d7914e7070
Pavel Kulakov <pavel@debian-BULLSEYE-live-builder-AMD64>
2022-08-16 12:39:45 +0300

    Auth controller with view added

---
+++ Blog/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ Blog/ViewModels/LoginViewModel.cs
@@ -0,0 +1,7 @@
+namespace Blog.ViewModels;
+
+public class LoginViewModel
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
